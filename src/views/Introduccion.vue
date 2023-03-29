946636d7249eea2da887aa806ca24f2821e03f31
ricardog07 <71521082+ricardog07@users.noreply.github.com>
2023-03-29 12:59:29 -0500

    Se vinculan videos.

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -25,7 +25,7 @@
       .col-lg-12.mt-4
         figure
         .video
-          iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+          iframe(width="560" height="315" src="https://www.youtube.com/embed/OwiJ8XUc9OE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
         
 
 
--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -248,7 +248,7 @@
       .col-lg-12.mt-4
         figure
         .video
-          iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+          iframe(width="560" height="315" src="https://www.youtube.com/embed/uvLAqgwhREo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
         
 
 
